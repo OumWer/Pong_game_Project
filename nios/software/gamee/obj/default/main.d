490707f5897bca495bbfa9afc0dd30af825212b2
Oumaima Werghemmi <195959232+OumWer@users.noreply.github.com>
2025-06-04 13:29:10 +0200

    Add files via upload

---
+++ nios/software/gamee/obj/default/main.d
@@ -0,0 +1,13 @@
+obj/default/main.o: main.c ../gamee_bsp/system.h ../gamee_bsp/linker.h \
+ ../gamee_bsp//HAL/inc/io.h ../gamee_bsp//HAL/inc/alt_types.h \
+ ../gamee_bsp//HAL/inc/alt_types.h
+
+../gamee_bsp/system.h:
+
+../gamee_bsp/linker.h:
+
+../gamee_bsp//HAL/inc/io.h:
+
+../gamee_bsp//HAL/inc/alt_types.h:
+
+../gamee_bsp//HAL/inc/alt_types.h:
